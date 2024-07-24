8440351e2165d6fafd19e2926687f9a5c9fc0624
Kevin J. Monter Zea. <kjms.15987@gmail.com>
2024-07-24 17:14:43 -0500

    Update 24-07-2024

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button> click me</Button>
+    </>
+  )
+}
+
+export default App
