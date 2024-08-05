45d4c6b7fb0c9af0c1a1bfc18193e33438086e6e
Kevin J. Monter Zea. <kjms.15987@gmail.com>
2024-08-05 17:20:33 -0500

    Update 05-08-2024

---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,15 @@
+import { useUpdateMyUser } from "../api/MyUserApi";
+import UserProfileForm from "../forms/user-profile-form/UserProfileForm";
+
+const UserProfilePage = () => {
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+
+
+  return (
+    <UserProfileForm 
+    onSave={updateUser}
+    isLoading={isUpdateLoading}/>
+  );
+};
+
+export default UserProfilePage;
