f980b9a97cba05a637a5644e9edc6f831692c6d3
Kevin J. Monter Zea. <kjms.15987@gmail.com>
2024-08-07 16:03:48 -0500

    Update 07-08-2024

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const AppRoutes = () => {
     return(
@@ -13,12 +14,17 @@ const AppRoutes = () => {
                 </Layout>
                 }/>
             <Route path="/auth-callback" element={<AuthCallbackPage />}/>
+
+            <Route element={<ProtectedRoute />}>
             <Route 
             path="/user-profile" 
             element={
             <Layout> 
                 <UserProfilePage /> 
             </Layout>}/>
+            
+            </Route>
+            
             <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
     )
