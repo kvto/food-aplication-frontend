3fe0f945da77726a16f64fb001a3f310e9da8884
Kevin J. Monter Zea. <kjms.15987@gmail.com>
2024-08-28 17:24:02 -0500

    Update 28-08-2024

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,10 +1,79 @@
 import { useParams } from "react-router-dom"
 import { useGetRestaurant } from "../api/RestaurantApi";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import RestaurantInfo from "../components/RestaurantInfo";
+import MenuItem from "../components/MenuItem";
+import { useState } from "react";
+import { Card } from "../components/ui/card";
+import OrderSummary from "../components/OrderSummary";
+import { MenuItem as MenuItemType } from "../types";
+
+export type CartItem = {
+    _id: string;
+    name: string;
+    price: number;
+    quantity: number;
+  };
+
 
 const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isLoading} = useGetRestaurant(restaurantId);
+    const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+        const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
+        return storedCartItems ? JSON.parse(storedCartItems) : [];
+      });
+
+      const addToCart = (menuItem: MenuItemType) => {
+        setCartItems((prevCartItems) => {
+          const existingCartItem = prevCartItems.find(
+            (cartItem) => cartItem._id === menuItem._id
+          );
+    
+          let updatedCartItems;
+    
+          if (existingCartItem) {
+            updatedCartItems = prevCartItems.map((cartItem) =>
+              cartItem._id === menuItem._id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+            );
+          } else {
+            updatedCartItems = [
+              ...prevCartItems,
+              {
+                _id: menuItem._id,
+                name: menuItem.name,
+                price: menuItem.price,
+                quantity: 1,
+              },
+            ];
+          }
+    
+          sessionStorage.setItem(
+            `cartItems-${restaurantId}`,
+            JSON.stringify(updatedCartItems)
+          );
+    
+          return updatedCartItems;
+        });
+      };
+
+      const removeFromCart = (cartItem: CartItem) => {
+        setCartItems((prevCartItems) => {
+          const updatedCartItems = prevCartItems.filter(
+            (item) => cartItem._id !== item._id
+          );
+    
+          sessionStorage.setItem(
+            `cartItems-${restaurantId}`,
+            JSON.stringify(updatedCartItems)
+          );
+    
+          return updatedCartItems;
+        });
+      };
+    
 
     if(isLoading || !restaurant){
         return "Loading..."
@@ -15,9 +84,28 @@ return (
     <div className="flex flex-col gap-10">
         <AspectRatio ratio={16/5}>
             <img 
-            src={restaurant.lastUpdated}
+            src={restaurant.imageUrl}
             className="rounded-md object-cover h-full w-full"/>
         </AspectRatio>
+        <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
+            <div className="flex flex-col gap-4">
+            <RestaurantInfo restaurant={restaurant}/>
+            <span className="text-2xl font-bold tracking-tight">Menu</span>
+            {restaurant.menuItems.map((menuItem) => (
+            <MenuItem
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
+          ))}
+            </div>  
+            <Card>
+            <OrderSummary
+              restaurant={restaurant}
+              cartItems={cartItems}
+              removeFromCart={removeFromCart}
+            />
+          </Card>
+        </div>
     </div>
 )
 }
